b4c456114dc16fbb9c3c1831ab6eeb717b4b6289
HectorVergel <77626779+HectorVergel@users.noreply.github.com>
2023-04-02 12:25:53 +0200

    Add files via upload

---
+++ Save&Load_System/SaveTest.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class SaveTest : MonoBehaviour, ISaveData
+{
+
+    Dictionary<string, bool> coin = new Dictionary<string, bool>();
+    public int deathCount;
+    public string scene;
+
+    
+
+    private void Start()
+    {
+        coin = GameDataManager.Load<Dictionary<string, bool>>("coinsCollected");
+        Debug.Log(deathCount);
+    }
+
+    private void OnEnable()
+    {
+        Subscribe();
+    }
+
+    private void OnDisable()
+    {
+        Unsubscribe();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            coin.TryAdd("pepe" + deathCount, true);
+            deathCount++;
+
+            
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+    }
+
+    public void SaveData()
+    {
+        GameDataManager.Save("coinsCollected", coin);
+    }
+
+    public void Subscribe()
+    {
+        GameDataManager.Add(this);
+    }
+
+    public void Unsubscribe()
+    {
+        GameDataManager.Remove(this);
+    }
+}
